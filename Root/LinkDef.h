8eb2ef68d2466664b4f5fa7b913c9c110dd97a58
iconnell <iconnell@cern.ch>
2017-03-23 14:41:10 +0000

    IC: Adding CMakeLists.txt and updating LinkDef in TtResonanceTools

--- Root/LinkDef.h
+++ Root/LinkDef.h
@@ -2,6 +2,9 @@
 #include "TtResonancesTools/Chi2Selector.h"
 #ifdef __CINT__
 
+#pragma extra_include "TtResonancesTools/TtresChi2.h";
+#pragma extra_include "TtResonancesTools/Chi2Selector.h";
+
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
